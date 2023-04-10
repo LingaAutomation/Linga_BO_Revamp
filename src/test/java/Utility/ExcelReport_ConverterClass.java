e745fd0b4cf023d306baebd424b154a6c54201d1
LingaAutomation <ramkumarm@lingaros.com>
2023-04-10 15:12:28 +0530

    Fourth Commit

--- src/test/java/Utility/ExcelReport_ConverterClass.java
+++ src/test/java/Utility/ExcelReport_ConverterClass.java
@@ -6,7 +6,7 @@ public class ExcelReport_ConverterClass {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		Xl.generateReport("C:\\Users\\raja.s\\Desktop\\Automation Reports\\Excel Report", "Product_and_Items.xlsx");
+		Xl.generateReport("C:\\Automation\\Linga_BO_Revamp\\Excel Report - Automation\\Excel Report", "Product_and_Items.xlsx");
 	}
 
 }
